2bee02fafbf71ffd7159fc241a7888a367b5ef34
jawaedan20 <m.jawahiruzzaman@gmail.com>
2021-10-30 14:54:28 +0700

    cart view

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,7 +1,49 @@
 @extends('layout.main')
 
 @section('container')
+<section class="d-flex mt-5">
+    <div class="item-cart me-5">
+        <div class="shadow p-3 bg-body rounded ">
+            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+                <b>Select All</b>
+            </label>
+        </div>
+        <div class="mt-3 shadow p-3 bg-body rounded d-flex align-items-center justify-content-between">
+            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+            <img src="/download.jfif" style="width:100px;height:100px" alt="product">
+            <div style="flex: 1;">
+                <p>sapatu bagsu langsung beli lah</p>
+                <p>Rp900.000</p>
+            </div>
+            <div>
+                <i data-feather="trash-2"></i>
+                <i data-feather="heart"></i>
+            </div>
+        </div>
+    </div>
+    <div class="summary shadow p-3 bg-body rounded">
+        <h4 class="mb-4">Summary</h4>
+        <div class="mb-2 item d-flex justify-content-between">
+            <p>Total Harga</p>
+            <p>Rp200.000</p>
+        </div>
+        <div class="mb-2 item d-flex justify-content-between">
+            <p>Total diskon</p>
+            <p>Rp200.000</p>
+        </div>
+        <div class="mb-2 item d-flex justify-content-between">
+            <p>Ongkir</p>
+            <p>Rp200.000</p>
+        </div>
+        <hr>
+        <div class=" mb-3  d-flex justify-content-between">
+            <b>Total Harga</b>
+            <b>Rp200.000</b>
+        </div>
+        <button class="check btn btn-primary" style="width:100%">Checkout</button>
+    </div>
+</section>
 
-    <h1>Cart Page</h1>
 
 @endsection
