4a72712644e813ac2229c3105b33ad96b7c1337b
alfathin <alfathin365@gmail.com>
2021-12-17 14:17:23 +0700

    footer

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -9,6 +9,7 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link href="/css/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="/css/footers.css">
   @yield('style')
 
   <title>We Shop | {{ $title }}</title>
@@ -84,6 +85,9 @@
   <div class="container mt-3">
     @yield('container')
   </div>
+
+  {{-- Footer --}}
+  @include('layout.footer')
 
 
   <script src="https://unpkg.com/feather-icons"></script>
